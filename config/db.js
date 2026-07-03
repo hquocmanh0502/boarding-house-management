f90154c9ea61495c9b918d7241d15cf2097c37eb
22014611-dot <22014611@st.phenikaa-uni.edu.vn>
2026-07-03 20:48:51 +0700

    db.js

--- config/db.js
+++ config/db.js
@@ -1,21 +1,13 @@
-const mongoose = require("mongoose");
-const dns = require("dns");
-
-//  FIX DNS TRƯỚC KHI CONNECT
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    console.log("DNS:", dns.getServers());
-
     await mongoose.connect(process.env.MONGO_URI);
-
-    console.log(" MongoDB connected");
+    console.log('✅ MongoDB Atlas connected!');
   } catch (err) {
-    console.error(" MongoDB error:", err.message);
+    console.error('❌ Kết nối MongoDB thất bại:', err.message);
     process.exit(1);
   }
-  
 };
 
 module.exports = connectDB;
